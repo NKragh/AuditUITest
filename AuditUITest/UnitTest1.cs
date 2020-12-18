56a0ac287be0558122d33cb449e7bf1a4340f862
NKragh <39466838+NKragh@users.noreply.github.com>
2020-12-18 15:25:51 +0100

    4 Test implemented

--- AuditUITest/UnitTest1.cs
+++ AuditUITest/UnitTest1.cs
@@ -1,24 +1,117 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Fiddler;
+using Proxy = OpenQA.Selenium.Proxy;
 
 namespace AuditUITest
 {
     [TestClass]
     public class UnitTest1
     {
+        //private string URL = "https://auditsystem.azurewebsites.net/";
+        private string URL = "http://127.0.0.1:5500/";
+
         [TestMethod]
-        public void TestMethod1()
+        public void HeadersPresent()
         {
             using (IWebDriver driver = new ChromeDriver(Directory.GetCurrentDirectory()))
             {
-                Uri url = new Uri("https://auditsystem.azurewebsites.net/");
+                Uri url = new Uri(URL);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
                 driver.Navigate().GoToUrl(url);
-                IReadOnlyCollection<IWebElement> elements = driver.FindElements(By.Name("checklist"));
-                Assert.IsNotNull(elements);
+
+                driver.FindElement(By.TagName("a")).Click();
+
+                IWebElement element = wait.Until(e => e.FindElement(By.Name("checklist")));
+
+                Assert.IsNotNull(element);
+            }
+        }
+
+        [TestMethod]
+        public void QuestionsLoaded()
+        {
+            using (IWebDriver driver = new ChromeDriver(Directory.GetCurrentDirectory()))
+            {
+                Uri url = new Uri(URL);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                driver.Navigate().GoToUrl(url);
+
+                driver.FindElement(By.TagName("a")).Click();
+
+                IWebElement element = wait.Until(e => e.FindElement(By.Name("checklist")));
+
+                element.Click();
+
+                IWebElement container = wait.Until(e => e.FindElement(By.Id("checklistcontainer")));
+                IWebElement question = container.FindElement(By.CssSelector(".row"));
+                Assert.IsTrue(question.Displayed);
+            }
+
+        }
+
+        [TestMethod]
+        public void AnswersClicked()
+        {
+            using (IWebDriver driver = new ChromeDriver(Directory.GetCurrentDirectory()))
+            {
+                Uri url = new Uri(URL + "audit.html");
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                driver.Navigate().GoToUrl(url);
+
+                IWebElement element = wait.Until(e => e.FindElement(By.Name("checklist")));
+
+                element.Click();
+
+                IWebElement container = wait.Until(e => e.FindElement(By.Id("checklistcontainer")));
+
+                IWebElement question = container.FindElement(By.CssSelector(".row"));
+
+                IWebElement radiobutton = question.FindElement(By.Name("main1"));
+
+                Assert.IsTrue(radiobutton.Enabled);
+                radiobutton.Click();
+                Assert.IsTrue(radiobutton.Selected);
+
+                //GetCssValue("value") == "OK"
+            }
+        }
+
+        [TestMethod]
+        public void SaveClicked()
+        {
+            using (IWebDriver driver = new ChromeDriver(Directory.GetCurrentDirectory()))
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+                Uri url = new Uri(URL + "audit.html");
+                driver.Navigate().GoToUrl(url);
+
+                IWebElement element = wait.Until(e => e.FindElement(By.Name("checklist")));
+
+                element.Click();
+
+                IWebElement container = wait.Until(e => e.FindElement(By.Id("checklistcontainer")));
+
+                IWebElement question = container.FindElement(By.CssSelector(".row"));
+
+                IWebElement radiobutton = question.FindElement(By.Name("main1"));
+
+                radiobutton.Click();
+
+                IWebElement savebutton = driver.FindElement(By.Id("savebtn"));
+
+                savebutton.Click();
+
+                string response = wait.Until(e => e.FindElement(By.Id("response")).Text);
+
+                Assert.AreEqual("Svar gemt succesfuldt.", response);
             }
         }
     }
